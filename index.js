2a4f3ef2039f625f8a9e00ea30057ae32dc7972e
adryannegorden <adryannegorden@gmail.com>
2023-03-18 00:58:30 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride > 2500) {
+    return 'No can do.';
+  } else if (ride > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (ride > 400) {
+    return 'That will be twenty bucks.';
+  } else {
+    return 'This one is on me!';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.'
+      break;
+    case "not as generous":
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+  }
 }
